f85bacf91f91d5eef7a8bc6d5985215da3e9deb6
Igor Patrick Ponticelli <patrickigor.ip@gmail.com>
2021-10-06 17:09:07 -0300

    Add responsive site

--- script.js
+++
@@ -1,8 +0,0 @@
-const btnMenu = document.getElementById('btn-mobile'); //
-
-function toggleMenu(){
-const nav = document.getElementById('nav'); //
-nav.classList.toggle('active');
-}
-
-btnMenu.addEventListener('click', toggleMenu);

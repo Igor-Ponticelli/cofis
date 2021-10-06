d69beff4b30e70e24bbeea4a64e565171d28e5a3
Igor Patrick Ponticelli <patrickigor.ip@gmail.com>
2021-10-05 21:40:03 -0300

    trying

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const btnMenu = document.getElementById('menu'); //
+const btnMenu = document.getElementById('btn-mobile'); //
 
 function toggleMenu(){
 const nav = document.getElementById('nav'); //
-nav.classList.toggle('active')
+nav.classList.toggle('active');
 }
 
 btnMenu.addEventListener('click', toggleMenu);
